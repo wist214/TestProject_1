04aaf76720fa2348f157080e68fa40280aad1833
wist214 <wist214@gmail.com>
2025-02-16 16:27:28 +0200

    Add LogException

---
+++ GameTipsShop/Solutions/SharedLibrary/GameTipsShop.SharedLibrarySolution/GameTipsShop.SharedLibrary/Logs/LogException.cs
@@ -0,0 +1,18 @@
+﻿using Serilog;
+
+namespace GameTipsShop.SharedLibrary.Logs
+{
+    public static class LogException
+    {
+        public static void LogExceptions(Exception ex, string message = null)
+        {
+            LogToFile($"{message} {ex.Message}");
+            LogToConsole($"{message} {ex.Message}");
+            LogToDebug($"{message} {ex.Message}");
+        }
+
+        private static void LogToFile(string message) => Log.Information(message);
+        private static void LogToConsole(string message) => Log.Warning(message);
+        private static void LogToDebug(string message) => Log.Debug(message);
+    }
+}
